0c757d423ad40b54c15b089c23b7b5b3e4499ff3
wakabat <wakabat@protonmail.com>
2025-04-23 07:24:34 +0000

    RISC-V tweaks in LLVM engine
    Note that simple turning on rv32 target can be trivial in LLVM setting,
    all we need is just tweaking a few configs. However, this change also
    comes with several bug fixes that are required in rv32 (possibly other
    32 bit target targets as well), and in rv64:
    
    * Certain pointer width are hardcoded to be 8, which will break on
    32-bit platforms
    * Metadata require an alignment of 8
    * RISC-V relocations are not properly handled
    * Ensure rv[32|64]im is used. Floating-point operations are disabled
    * Certain floating-point & 64-bit routine functions are required in
    libcalls
    * Previous transmute-based way of setting ABI in rv64 platform would
    crash, this change also fixes it
    * Trampoline impelmentation in rv32

--- lib/compiler-llvm/src/config.rs
+++ lib/compiler-llvm/src/config.rs
@@ -244,6 +244,14 @@ impl LLVM {
                 info: true,
                 machine_code: true,
             }),
+            Architecture::Riscv32(_) => InkwellTarget::initialize_riscv(&InitializationConfig {
+                asm_parser: true,
+                asm_printer: true,
+                base: true,
+                disassembler: true,
+                info: true,
+                machine_code: true,
+            }),
             Architecture::LoongArch64 => {
                 InkwellTarget::initialize_loongarch(&InitializationConfig {
                     asm_parser: true,
@@ -275,26 +283,32 @@ impl LLVM {
 
         let target_triple = self.target_triple(target);
         let llvm_target = InkwellTarget::from_triple(&target_triple).unwrap();
-        let mut llvm_target_machine_options = TargetMachineOptions::new()
+        let llvm_target_machine_options = TargetMachineOptions::new()
             .set_cpu(match triple.architecture {
                 Architecture::Riscv64(_) => "generic-rv64",
+                Architecture::Riscv32(_) => "generic-rv32",
                 Architecture::LoongArch64 => "generic-la64",
                 _ => "generic",
             })
             .set_features(match triple.architecture {
-                Architecture::Riscv64(_) => "+m,+a,+c,+d,+f",
+                // Our RISC-V experiment only uses rv[32|64]im. Floating point, compressed
+                // instruction, atomic operations are not supported.
+                Architecture::Riscv64(_) => "+m",
+                Architecture::Riscv32(_) => "+m",
                 Architecture::LoongArch64 => "+f,+d",
                 _ => &llvm_cpu_features,
             })
             .set_level(self.opt_level)
             .set_reloc_mode(self.reloc_mode(self.target_binary_format(target)))
             .set_code_model(match triple.architecture {
-                Architecture::LoongArch64 | Architecture::Riscv64(_) => CodeModel::Medium,
+                Architecture::LoongArch64 | Architecture::Riscv64(_) | Architecture::Riscv32(_) => {
+                    CodeModel::Medium
+                }
                 _ => self.code_model(self.target_binary_format(target)),
             });
-        if let Architecture::Riscv64(_) = triple.architecture {
-            llvm_target_machine_options = llvm_target_machine_options.set_abi("lp64d");
-        }
+        // if let Architecture::Riscv64(_) = triple.architecture {
+        //     llvm_target_machine_options = llvm_target_machine_options.set_abi("lp64d");
+        // }
         llvm_target
             .create_target_machine_from_options(&target_triple, llvm_target_machine_options)
             .unwrap()
--- lib/vm/src/mmap.rs
+++ lib/vm/src/mmap.rs
@@ -80,7 +80,7 @@ impl Mmap {
 
         assert!(backing_file.is_none());
 
-        use std::alloc::{alloc, Layout};
+        use std::alloc::{Layout, alloc};
 
         // mmap requires alignment to pages, we follow the same behavior
         let layout = Layout::from_size_align(mapping_size, page_size).unwrap();
@@ -457,7 +457,7 @@ impl Drop for Mmap {
             }
             .expect("restore memory as accessible again");
 
-            use std::alloc::{dealloc, Layout};
+            use std::alloc::{Layout, dealloc};
 
             let layout = Layout::from_size_align(self.total_size, region::page::size()).unwrap();
             unsafe { dealloc(self.ptr as *mut u8, layout) }
