ee9f56192d386a363daa06a81e05f571fdde8ca1
wakabat <wakabat@protonmail.com>
2025-04-23 07:24:34 +0000

    RISC-V tweaks in LLVM engine
    Note that simple turning on rv32 target can be trivial in LLVM setting,
    all we need is just tweaking a few configs. However, this change also
    comes with several bug fixes that are required in rv32 (possibly other
    32 bit target targets as well), and in rv64:
    
    * Certain pointer width are hardcoded to be 8, which will break on
    32-bit platforms
    * Metadata require an alignment of 8
    * RISC-V relocations are not properly handled
    * Ensure rv[32|64]im is used. Floating-point operations are disabled
    * Certain floating-point & 64-bit routine functions are required in
    libcalls
    * Previous transmute-based way of setting ABI in rv64 platform would
    crash, this change also fixes it
    * Trampoline impelmentation in rv32

--- lib/compiler-llvm/src/translator/intrinsics.rs
+++ lib/compiler-llvm/src/translator/intrinsics.rs
@@ -1249,6 +1249,7 @@ impl<'ctx, 'a> CtxType<'ctx, 'a> {
         cache_builder: &'a Builder<'ctx>,
         abi: &'a dyn Abi,
         config: &'a LLVM,
+        pointer_width: u8,
     ) -> CtxType<'ctx, 'a> {
         CtxType {
             config,
@@ -1265,8 +1266,7 @@ impl<'ctx, 'a> CtxType<'ctx, 'a> {
             cached_functions: HashMap::new(),
             cached_memory_op: HashMap::new(),
 
-            // TODO: pointer width
-            offsets: VMOffsets::new(8, wasm_module),
+            offsets: VMOffsets::new(pointer_width, wasm_module),
         }
     }
 
