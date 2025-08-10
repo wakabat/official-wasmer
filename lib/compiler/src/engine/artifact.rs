fe503d4fdf622fb0721175107cf665c55cf43a0e
wakabat <wakabat@protonmail.com>
2025-08-10 06:49:54 +0000

    Expose function to dump instantiated function addresses and lengths

--- lib/compiler/src/engine/artifact.rs
+++ lib/compiler/src/engine/artifact.rs
@@ -719,24 +719,7 @@ impl Artifact {
             return Ok(()); // already done
         }
 
-        let finished_function_extents = self
-            .allocated
-            .as_ref()
-            .expect("It must be allocated")
-            .finished_functions
-            .values()
-            .copied()
-            .zip(
-                self.allocated
-                    .as_ref()
-                    .expect("It must be allocated")
-                    .finished_function_lengths
-                    .values()
-                    .copied(),
-            )
-            .map(|(ptr, length)| FunctionExtent { ptr, length })
-            .collect::<PrimaryMap<LocalFunctionIndex, _>>()
-            .into_boxed_slice();
+        let finished_function_extents = self.finished_function_extents();
 
         let frame_info_registration = &mut self
             .allocated
@@ -781,6 +764,27 @@ impl Artifact {
             .as_ref()
             .expect("It must be allocated")
             .finished_functions
+    }
+
+    /// Returns function extents for external processing, e.g., debugging & profiling
+    pub fn finished_function_extents(&self) -> BoxedSlice<LocalFunctionIndex, FunctionExtent> {
+        self.allocated
+            .as_ref()
+            .expect("It must be allocated")
+            .finished_functions
+            .values()
+            .copied()
+            .zip(
+                self.allocated
+                    .as_ref()
+                    .expect("It must be allocated")
+                    .finished_function_lengths
+                    .values()
+                    .copied(),
+            )
+            .map(|(ptr, length)| FunctionExtent { ptr, length })
+            .collect::<PrimaryMap<LocalFunctionIndex, _>>()
+            .into_boxed_slice()
     }
 
     /// Returns the function call trampolines allocated in memory of this
